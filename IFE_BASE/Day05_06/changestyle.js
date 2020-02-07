bad982a27fcdbbfd0041c1c8b4c4837136699841
keviness <kevin1737@163.com>
2020-02-07 21:02:57 +0800

    add layout exercise modify changestyle.js

--- IFE_BASE/Day05_06/changestyle.js
+++ IFE_BASE/Day05_06/changestyle.js
@@ -1,6 +1,27 @@
 /*change my resume style*/
+function addLoadEvent(func){
+    var oldonload = window.onload;
+    if (typeof window.onload != "function"){
+        window.onload = fun;
+    }else{
+        window.onload = function(){
+            oldonload();
+            func();
+        }
+    }
+}
+function prepareStyle(){
+    var style = document.getElementsByTagName("a");
+    for (var i=0; i<style.length; i++){
+        style[i].onclick = function(){
+            return changeStyle(this);
+        }
+    }
+}
 function changeStyle(whichStyle){
     var source = whichStyle.getAttribute("href")
     var target = document.getElementsByTagName("link")[0];
     target.setAttribute("href", source);
+    return false;
 }
+addLoadEvent(prepareStyle);
