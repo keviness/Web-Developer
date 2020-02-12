6e33aa3f3cb0ceba8e76cbf701ce4c6ce38b728b
keviness <kevin1737@163.com>
2020-02-12 23:51:18 +0800

    mofify the index.html and add laout2.css

--- IFE_BASE/Day07_08/changeStyle.js
+++ IFE_BASE/Day07_08/changeStyle.js
@@ -1,6 +1,29 @@
+/*--seperate the javascript and html document--*/
+function addOnloadEvent(func){
+    var oldonload = window.onload;
+    if (typeof window.onload != "function"){
+        window.onload = func;
+        alert(typeof func);
+    }else{
+        window.onload = function(){
+            oldonload();
+            func();
+        }
+    }
+}
+function prepareStyle(){
+    var li = document.getElementsByClassName("change")[0];
+    var style = li.getElementsByTagName("a");
+    for (var i=0; i<style.length; i++){
+        style[i].onclick = function(){
+            return changeStyle(this);
+        }
+    }
+}
 function changeStyle(whichStyle){
     var source = whichStyle.getAttribute("href");
     var link = document.getElementsByTagName("link")[0];
     link.setAttribute("href", source);
     return false;
 }
+addOnloadEvent(prepareStyle);
