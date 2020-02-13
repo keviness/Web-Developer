8d5b0d6490bed7dabdc97f91340b4573eb624721
keviness <kevin1737@163.com>
2020-02-13 11:22:37 +0800

    modify the index.html and add changestyle.js

---
+++ IFE_BASE/Day07_08/layout/changeStyles.js
@@ -0,0 +1,32 @@
+/*---change the layout styles---*/
+function addOnloadEvent(func){
+    var oldonload = window.onload;
+    if (typeof window.onload != "function"){
+        window.onload = func;
+    }else{
+        window.onload = function(){
+            oldonload();
+            func();
+        }
+    }
+}
+function prepareStyle(){
+    if (!document.getElementsByClassName) return false;
+    if(!document.getElementsByClassName("changeStyle")) return false;
+    var style_list = document.getElementsByClassName("changeStyle")[0];
+    var styles = style_list.getElementsByTagName("a");
+    for (var i=0; i<styles.length; i++){
+        styles[i].onclick = function(){
+            return showStyles(this);
+        }
+    }
+}
+function showStyles(whichStyle){
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementsByName("link")) return false;
+    var source = whichStyle.getAttribute("href");
+    var target = document.getElementsByTagName("link")[0];
+    target.setAttribute("href", source);
+    return false;
+}
+addOnloadEvent(prepareStyle);
